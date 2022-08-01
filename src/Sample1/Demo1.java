fcb65a24ae728777e746598a071b18eec5440d7c
lenovo <lenovo@Manisha>
2022-08-01 15:00:38 +0530

    <Manisha> new file

--- src/Sample1/Demo1.java
+++ src/Sample1/Demo1.java
@@ -1,10 +1,22 @@
 package Sample1;
 
+import java.util.Scanner;
+
 public class Demo1 
 {
 	public static void main(String[] args) {
 		
-		System.out.println("Hi");
+		System.out.println("Hi Yogesh new changes ");
+		System.out.println("Enter no.:");
+		Scanner scan =new Scanner(System.in);
+		int num = scan.nextInt();
+		int fact=1;
+		for (int i = num; i>=1; i--) 
+		{
+			fact=fact*i;
+			
+		}
+		System.out.println(fact);
 	}
 
 }
