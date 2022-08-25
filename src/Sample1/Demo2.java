3d5297f8904a669d633326fe681f4a70bee38d87
yogesh4151 <95487956+yogesh4151@users.noreply.github.com>
2022-08-25 07:01:24 -0700

    Update Demo2.java

--- src/Sample1/Demo2.java
+++ src/Sample1/Demo2.java
@@ -6,6 +6,8 @@ public class Demo2 {
 		
 		System.out.println("Test Jenkins");
 		System.out.println("Good Morning");
+		System.out.println("Jnkins changes from leneovo pc");
+
 		
 		System.out.println("Test Git commit and jenkins");
 		System.out.println("Test Jenkins");
